9fc84dfd0f852f86e858bdd0b3e369abab6af081
kevinlant <kevin.m.lant@gmail.com>
2023-08-01 23:28:01 -0400

    Add workout cards

--- app/logs/page.jsx
+++ app/logs/page.jsx
@@ -1,27 +1,72 @@
 "use client";
 
+import { Card, Divider } from "antd";
 import PrimaryButton from "../components/PrimaryButton";
 import styled from "styled-components";
+import { useRouter } from "next/navigation";
+
+const workouts = ["Chest Day", "Back Day", "Leg Day", "Shoulder Day"];
 
 const Logs = () => {
+  const router = useRouter();
+
   return (
-    <StyledMainContainer>
-      <StyledContentContainer></StyledContentContainer>
-      <PrimaryButton label="Start Workout"></PrimaryButton>
-    </StyledMainContainer>
+    <>
+      <StyledContentContainer>
+        {workouts.map((workout, index) => (
+          <StyledCard
+            bordered={false}
+            hoverable
+            key={index}
+            title={workout}
+            className="mat-elevation-z3"
+            headStyle={{ color: "#232323" }}
+          >
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </StyledCard>
+        ))}
+      </StyledContentContainer>
+      <Divider></Divider>
+      <PrimaryButton
+        label="Start Workout"
+        onClick={() => {
+          router.push("/logs/somethingRandom");
+        }}
+      ></PrimaryButton>
+    </>
   );
 };
 
-const StyledMainContainer = styled.div`
-  display: flex;
+const StyledContentContainer = styled.div`
+  display: inline-flex;
   flex-direction: column;
-  align-content: stretch;
-  padding: 10px;
   flex-grow: 1;
+  gap: 10px;
+  overflow: auto;
+  height: 1px;
+
+  @media only screen and (min-width: 576px) {
+    --gap: 10px;
+    display: inline-flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    margin: calc(-1 * var(--gap)) 0 0 calc(-1 * var(--gap));
+    width: calc(100% + var(--gap));
+    gap: 0px;
+  }
 `;
 
-const StyledContentContainer = styled.div`
-  flex-grow: 1;
+const StyledCard = styled(Card)`
+  margin: var(--gap) 0 0 var(--gap);
+  width: 100%;
+  background: white;
+  color: #232323;
+
+  @media only screen and (min-width: 576px) {
+    width: calc(50% - 10px);
+  }
 `;
 
 export default Logs;
